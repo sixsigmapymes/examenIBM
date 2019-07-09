52de7fbc0e831c333862bbdae0b4987fab720b3e
sixsigmapymes <trabajo08@gmail.com>
2019-07-09 15:46:29 -0300

    Delete personaTest.sql

--- src/test/resources/personaTest.sql
+++
@@ -1,5 +0,0 @@
-TRUNCATE  `persona`;
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (1, 'dani', 'Norway', 45,'HOMBRE');
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (2, 'juana', 'Borway', 33,'MUJER');
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (3, 'xani', 'Hin', 28,'MUJER');
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (4, 'gabriel', 'Noray', 75,'HOMBRE');
