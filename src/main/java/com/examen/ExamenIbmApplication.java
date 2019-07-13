2d3d5bd93df72ba9b175d4d773d7bd382e9e625c
Sergio Rozenberg <trabajo08@gmail.com>
2019-07-13 16:57:10 -0300

    app

---
+++ src/main/java/com/examen/ExamenIbmApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenIbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenIbmApplication.class, args);
+	}
+
+}
