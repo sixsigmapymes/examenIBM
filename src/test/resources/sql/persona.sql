5103958cfb5be18cf03a5a0c6f52aa794ca92514
Sergio Rozenberg <trabajo08@gmail.com>
2019-07-13 17:43:06 -0300

    script

--- src/test/resources/sql/persona.sql
+++ src/test/resources/sql/persona.sql
@@ -1,11 +1,14 @@
-TRUNCATE  `persona`;
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (1, 'dani', 'Norway', 45,'HOMBRE');
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (2, 'juana', 'Borway', 33,'MUJER');
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (3, 'xani', 'Hin', 28,'MUJER');
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (4, 'gabriel', 'Noray', 75,'HOMBRE');
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (5, 'dani', 'Norway', 45,'HOMBRE');
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (6, 'juana', 'Borway', 33,'MUJER');
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (7, 'jana', 'Jorzt', 28,'MUJER');
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (8, 'gabriela', 'ware', 75,'MUJER');
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (9, 'ana', 'Herz', 28,'MUJER');
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (10, 'juan', 'Garcia', 34,'HOMBRE');
+DROP TABLE IF EXISTS PERSONA;
+CREATE TABLE PERSONA(ID INT PRIMARY KEY, nombre VARCHAR(255),apellido VARCHAR(255),edad LONG,sexo VARCHAR(30));
+
+DELETE  `PERSONA`;
+INSERT INTO `PERSONA` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (1, 'dani', 'Norway', 45,'HOMBRE');
+INSERT INTO `PERSONA` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (2, 'juana', 'Borway', 33,'MUJER');
+INSERT INTO `PERSONA` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (3, 'xani', 'Hin', 28,'MUJER');
+INSERT INTO `PERSONA` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (4, 'gabriel', 'Noray', 75,'HOMBRE');
+INSERT INTO `PERSONA` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (5, 'dani', 'Norway', 45,'HOMBRE');
+INSERT INTO `PERSONA` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (6, 'juana', 'Borway', 33,'MUJER');
+INSERT INTO `PERSONA` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (7, 'jana', 'Jorzt', 28,'MUJER');
+INSERT INTO `PERSONA` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (8, 'gabriela', 'ware', 75,'MUJER');
+INSERT INTO `PERSONA` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (9, 'ana', 'Herz', 28,'MUJER');
+INSERT INTO `PERSONA` (`id`, `nombre`, `apellido`, `edad`, `sexo`) VALUES (10, 'juan', 'Garcia', 34,'HOMBRE');
